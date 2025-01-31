5d9f451127cb1605d01163d33884b4d59aad2809
kronotitans <kronotitans@gmail.com>
2025-01-31 19:35:58 +0500

    basePath removed

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  basePath: "krono-titans-app/",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;

ae411935a998503a7c726150fb3da6d230a0bbe9
kronotitans <kronotitans@gmail.com>
2025-02-03 18:58:17 +0500

    contract card removed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import ContractCard from "./ContractCard";
+/// import ContractCard from "./ContractCard";
 
 const Hero = () => {
   return (
@@ -26,7 +26,7 @@ const Hero = () => {
             Get Ready for the Future of Gaming
           </p>
 
-          <ContractCard address="UQBuURAhP4rhEjNqLD94ox_py47osrXZW_SWY3Yp6-wzaFJf" />
+          {/* <ContractCard address="UQBuURAhP4rhEjNqLD94ox_py47osrXZW_SWY3Yp6-wzaFJf" /> */}
 
           <div className="flex justify-center gap-4">
             <button className="bg-gradient-to-r from-blue-500 to-purple-600 px-8 py-4 rounded-full text-lg font-medium hover:opacity-90 transition-opacity">
